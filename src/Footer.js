dbd7564cb712d88010bc018416f7cbe44c35cc0c
jamesnjugunah <njuguna.kariuki22@students.dkut.ac.ke>
2024-08-09 17:25:20 +0300

    update

--- src/Footer.js
+++ src/Footer.js
@@ -4,17 +4,6 @@ function Footer() {
   return (
     <footer id="footer">
       <div className="container">
-        <ul className="footer-links">
-          <li><a href="#hero">Home</a></li>
-          <li><a href="#about">About Us</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#contact">Contact Us</a></li>
-        </ul>
-        <ul className="social-links">
-          <li><a href="https://www.facebook.com/"><i className="fab fa-facebook"></i></a></li>
-          <li><a href="https://x.com/NJUGUNAJAM2003"><i className="fab fa-twitter"></i></a></li>
-        </ul>
         <p>&copy; 2024 Allied Contracting Engineers. All Rights Reserved.</p>
       </div>
     </footer>
--- src/Navbar.js
+++ src/Navbar.js
@@ -8,15 +8,16 @@ function Header() {
         <img src={images} alt="Allied Contracting Engineers Logo" />
         <span>ALLIED CONSTRUCTION & ENGINEERING (ACE)</span>
       </div>
-      <nav>
-        <ul>
-          <li><a href="#hero">Home</a></li>
-          <li><a href="#about">About Us</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#contact">Contact Us</a></li>
-        </ul>
-      </nav>
+        <nav>
+          <ul>
+            <li><a href="#hero">Home</a></li>
+            <li><a href="#about">About Us</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact Us</a></li>
+          </ul>
+        </nav>
+     
     </header>
   );
 }

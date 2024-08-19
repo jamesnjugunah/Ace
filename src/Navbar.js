4298a875045a92be1b705abde07607d810a414f1
jamesnjugunah <njuguna.kariuki22@students.dkut.ac.ke>
2024-08-19 11:16:51 +0300

    update

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import images from './images/logo1.jpeg';
+import React, { useState } from 'react';
+import images from './images/logo4.webp';
 
-function Header() {
+function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className='head'>
       <div className="logo">
         <img src={images} alt="Allied Contracting Engineers Logo" />
         <span>ALLIED CONSTRUCTION & ENGINEERING (ACE)</span>
       </div>
-        <nav>
-          <ul>
-            <li><a href="#hero">Home</a></li>
-            <li><a href="#about">About Us</a></li>
-            <li><a href="#services">Services</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#contact">Contact Us</a></li>
-          </ul>
-        </nav>
-     
+      <nav>
+        <div className="menu-icon" onClick={toggleMenu}>
+          &#9776;
+        </div>
+        <ul className={isMenuOpen ? 'nav-list active' : 'nav-list'}>
+          <li><a href="#hero">Home</a></li> 
+          <li><a href="#about">About Us</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#contact">Contact Us</a></li>            
+        </ul>
+      </nav>
     </header>
   );
 }
 
-export default Header;
+export default Navbar;
